a4173ebcaccec785aec2fb528c5c8ec476222075
2424005179@qq.com <2424005179@qq.com>
2025-04-09 19:38:51 +0800

    feat: wujie-vue3

--- main/src/main.js
+++ main/src/main.js
@@ -1,8 +1,10 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import { a } from 'common'
+// import Wujie from 'wujie-vue3' 
+
 
 createApp(App).mount('#app')
 
