6b1962c4e650375eaea15649973bde07826dfbd6
2424005179@qq.com <2424005179@qq.com>
2025-04-12 18:29:56 +0800

    feat

--- main/src/main.js
+++ main/src/main.js
@@ -3,7 +3,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { a } from 'common'
-// import Wujie from 'wujie-vue3' 
+import Wujie from 'wujie-vue3' 
+
+const {preloadApp} = Wujie
+
+preloadApp({name: 'vue3', url: 'http://localhost:5175/', exec: true})
+preloadApp({name: 'react', url: 'http://localhost:5174/', exec: true})
 
 
 createApp(App).mount('#app')
